6dd8418283e290f1109f23aebced6510b39b9086
Sewoong-Lee <81853455+Sewoong-Lee@users.noreply.github.com>
2021-08-14 20:13:54 +0900

    20210814

---
+++ Team3_Movie/src/main/java/three/team/movie/dto/Mv_movie_reply.java
@@ -0,0 +1,22 @@
+package three.team.movie.dto;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class Mv_movie_reply {
+
+	private int mr_num;	
+	private int movie_num;	
+	private String user_id;	
+	private String content;	
+	private int like_cnt;	
+	private int dis_like_cnt;	
+	private String ip;	
+	private int re_step;	
+	private int re_level;
+	private Date reg_date;	
+	private Date modify_date;	
+
+}
